6fdde29e7ddb22b58837b28baa010ad1e4b056ed
Daniel <heesu07@gmail.com>
2021-03-19 09:06:06 +1300

    Add project files.

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder();
+
+            var mailService = new MailService();
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            
+            var messageService = new MessageService();
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EventsAndDelegates
+{    
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+    public class VideoEncoder
+    {
+        // 만들어서 사용하기
+        // 1. Define a delegate
+        // 2. Define an event based on that delegate
+        // 3. Raise the event
+
+        //public delegate void VideoEncodeEventHandler(object source, VideoEventArgs args);
+        //public event VideoEncodeEventHandler VideoEncoded;
+
+        // 제공되는 것 사용하기
+        // EventHandler
+        // EventHandler<TEventArgs>
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+        public void Encode(Video video )
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() { Video = video});
+            }
+        }
+    }
+}
